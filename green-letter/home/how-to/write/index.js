2e4d1cf12f2d54cd3672064154cee4ec86b3310c
判治珠美 <nankaowata@gmail.com>
2019-07-29 12:35:33 +0900

    手紙を書くページにアラートを付けました。

--- green-letter/home/how-to/write/index.js
+++ green-letter/home/how-to/write/index.js
@@ -113,5 +113,20 @@ window.addEventListener("load", init);
           
         }, false);
         
+        var sendBtn = document.getElementById('send-btn');
+        sendBtn.addEventListener('click', function() {
+  
+            console.log('送るのボタンです');
+            // alert('JavaScriptのアラート');
+            if( confirm("手紙を送りますか？") ) {
+                alert("手紙を送りました。");
+            }
+            else {
+                alert("手紙を送りませんでした。");
+            }
+          
+        }, false);
+        
     }
 // 鉛筆の描画終了
+
