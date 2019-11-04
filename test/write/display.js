8b55ca32ec0a0ce7afbc24dbb733920dc1939201
判治珠美 <nankaowata@gmail.com>
2019-11-04 15:56:09 +0900

    名前と年齢と性別がconsole.logに出るようになりました。

--- test/write/display.js
+++ test/write/display.js
@@ -1,41 +1,50 @@
 // 送ったデータをconsoleに表示させる
 
-const input = document.querySelector('#screen_name');
-const input = document.querySelector('#age');
+const screenName = document.querySelector('#screen_name');
+const ageInput = document.querySelector('#age');
 const select = document.querySelector('#info-sex3');
 const canvas = document.querySelector('#myCanvas');
 const form = document.querySelector('#send-btn');
 
+console.log(select);
+
 form.addEventListener('submit', function(e) {
+  console.log('送信が試みられました');
   // データの送信をキャンセルする
   e.preventDefault();
+
+  const name = screenName.value;
+  const sex = select.value;
+  const age = ageInput.value;
+
+  console.log(name, sex, age);
   
-  const url = form.action;
+  // const url = form.action;
   
-  // 入力された文字を取得する
-  const body = canvas.value;
+  // // 入力された文字を取得する
+  // const body = canvas.value;
   
-  // canvas に書き込まれた内容を
-  // base64（文字列）にしてbase64という変数に入れておく
-  const base64 = canvas.toDataURL('image/jpeg');
+  // // canvas に書き込まれた内容を
+  // // base64（文字列）にしてbase64という変数に入れておく
+  // const base64 = canvas.toDataURL('image/jpeg');
   
-	axios.post(url, {
-    body: body, // 送信するデータPHP側で$_POSST["body"]で受け取れる
-    base64: base64 // 送信するデータPHP側で$_POSST["base64"]で受け取れる
-  })
-  .then(function (response) {
-    // データの送信に成功したときの処理をここに書く
-  })
-  .catch(function (error) {
-    // データの送信に失敗したときの処理をここに書く
-  });
+	// axios.post(url, {
+  //   body: body, // 送信するデータPHP側で$_POSST["body"]で受け取れる
+  //   base64: base64 // 送信するデータPHP側で$_POSST["base64"]で受け取れる
+  // })
+  // .then(function (response) {
+  //   // データの送信に成功したときの処理をここに書く
+  // })
+  // .catch(function (error) {
+  //   // データの送信に失敗したときの処理をここに書く
+  // });
   
-  console.log(`
-  	${url} に以下のデータを送ります
-    ${body}
-    ${base64}
-   `);
+  // console.log(`
+  // 	${url} に以下のデータを送ります
+  //   ${body}
+  //   ${base64}
+  //  `);
    
-   console.log(form);
+  //  console.log(form);
   
 });
