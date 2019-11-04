3489460aca639b9c9d8b88a342577e291a8906ea
判治珠美 <nankaowata@gmail.com>
2019-11-04 16:26:45 +0900

    できました

--- test/write/display.js
+++ test/write/display.js
@@ -19,33 +19,39 @@ form.addEventListener('submit', function(e) {
 
   console.log(name, sex, age);
   
-  // const url = form.action;
+  const url = form.action;
   
   // // 入力された文字を取得する
-  // const body = canvas.value;
+  const body = canvas.value;
+  console.log(body);
   
   // canvas に書き込まれた内容を
   // base64（文字列）にしてbase64という変数に入れておく
   const base64 = canvas.toDataURL('image/jpeg');
   console.log(base64);
   
-	// axios.post(url, {
-  //   body: body, // 送信するデータPHP側で$_POSST["body"]で受け取れる
-  //   base64: base64 // 送信するデータPHP側で$_POSST["base64"]で受け取れる
-  // })
-  // .then(function (response) {
-  //   // データの送信に成功したときの処理をここに書く
-  // })
-  // .catch(function (error) {
-  //   // データの送信に失敗したときの処理をここに書く
-  // });
+	axios.post(url, {
+    name: name,
+    sex: sex,
+    age: age,
+    body: body, // 送信するデータPHP側で$_POSST["body"]で受け取れる
+    base64: base64 // 送信するデータPHP側で$_POSST["base64"]で受け取れる
+  })
+  .then(function (response) {
+    // データの送信に成功したときの処理をここに書く
+    console.log(response);
+  })
+  .catch(function (error) {
+    // データの送信に失敗したときの処理をここに書く
+    console.log(error);
+  });
   
-  // console.log(`
-  // 	${url} に以下のデータを送ります
-  //   ${body}
-  //   ${base64}
-  //  `);
+  console.log(`
+  	${url} に以下のデータを送ります
+    ${body}
+    ${base64}
+   `);
    
-  console.log(axios);
+  console.log(error);
   
 });
