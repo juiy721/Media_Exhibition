db9ab91f44092859d4a02a21a08628318c0c130f
判治珠美 <nankaowata@gmail.com>
2019-11-18 16:19:11 +0900

    手紙を選ぶページに保存した手紙の一行目を反映できるようにしました。

--- green-letter/choose/js/main.js
+++ green-letter/choose/js/main.js
@@ -4,13 +4,25 @@ axios.get('/api/get_letter.php')
 .then(function (response) {
 // データの送信に成功したときの処理をここに書く
     console.log(response);
-    console.log(response.data[0].img);
+    // console.log(response.data[i].img);
     var el = document.getElementById("test");
+    let html = '';
+    for (var i = 0; i < response.data.length; i++) {
+        console.log(response.data[i]);
+        html += `
+        <a href="../see?id=1/">
+        <div class="cut">
+            <img src="/../../api/${response.data[i].img}.jpeg"
+                 class="first-line" />
+        </div>
+        <img src="img/empty-frame.png"
+              class="green-frame"
+              alt="" />
+    </a>
+    `;
+ }
     el.innerHTML = `
-    <div>
-        <p>${response.data[0].img}</p>
-        <img src=${response.data[0].img} />
-    </div>
+    <div>${html}</div>
     `;
 })
 .catch(function (error) {
@@ -18,3 +30,4 @@ axios.get('/api/get_letter.php')
     console.log(error);
 });
 console.log(axios);
+// console.log(html);
---
+++ green-letter/see/js/main.js
@@ -0,0 +1,22 @@
+// データを取得する
+
+axios.get('/api/get_letter.php')
+.then(function (response) {
+// データの送信に成功したときの処理をここに書く
+    console.log(response);
+    console.log(response.data[0].img);
+    var el = document.getElementById("test");
+    el.innerHTML = `
+    <div>
+        <p>${response.data[0].img}</p>
+        <a href="../see?id=1/">
+            <img src="/../../api/${response.data[0].img}.jpeg" />
+        </a>
+    </div>
+    `;
+})
+.catch(function (error) {
+// データの送信に失敗したときの処理をここに書く
+    console.log(error);
+});
+console.log(axios);
